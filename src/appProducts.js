1146e2633510077f74bf751ca70f1907cadf9e40
Monica <monica.lombardi1970@gmail.com>
2026-01-30 22:55:09 -0300

    error con la instalacion de nodemon

---
+++ src/appProducts.js
@@ -0,0 +1,25 @@
+import express from 'express';
+const appProducts = express();
+const PORT = 8080;
+let id = 0;
+
+
+appProducts.get('/', (request, response) =>{
+    // el request es lo que pide el cliente por el momento esta pidien la raiz'/'
+    // depues puede estar pidiendo mas cosasc
+    //yo cada vez que hago un refrescar en el local
+    //se va aumentado el id en la terminal
+    id++;
+    console.log(`<h1>id products ${id}</h1>`);
+response.send('hola desde Express');
+});
+
+
+
+
+
+
+appProducts.listen(PORT, ()=> {
+   
+    console.log(`Servidor con Express ${PORT}`);
+})
